fad2b5673631c7a6a5147ca53636ee059c048c6b
Arthaud <arthaudp33@gmail.com>
2023-11-29 20:35:23 +0100

    feat: add blocquote custom component to body field

--- admin/app/fields/body.js
+++ admin/app/fields/body.js
@@ -4,7 +4,7 @@ export const body = {
   name: 'body',
   label: t.fields.body,
   widget: 'markdown',
-  editor_components: ['button', 'figure', 'twitter', 'youtube'],
+  editor_components: ['blockquote', 'button', 'figure', 'twitter', 'youtube'],
   required: false,
   i18n: true
 };
