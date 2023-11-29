03b801fb855c0dbf4f83c5b96e120acd02475497
sebousan <sebousan@gmail.com>
2023-11-29 14:56:37 +0100

    Blockquote preview

--- admin/app/editor/shortcodes/blockquote.js
+++ admin/app/editor/shortcodes/blockquote.js
@@ -6,27 +6,42 @@ import { image_src } from '../../fields/image-src.js'
 import { t } from "../../i18n/translater.js";
 
 CMS.registerEditorComponent({
-    id: 'blockquote',
-    label: t.shortcodes.blockquote,
-    fields: [
-        quote,
-        title,
-        text,
-        image_src
-    ],
-    pattern: /{{< blockquote quote="(.*?)" title="(.*?)" text="(.*?)" image="(.*?)" >}}/,
-    fromBlock: function (match) {
-        return {
-            quote: match[1] ?? '',
-            title: match[2] ?? '',
-            text: match[3] ?? '',
-            image: match[4] ?? ''
-        };
-    },
-    toBlock: function (obj) {
-        return `{{< blockquote quote="${obj.quote ?? ''}" title="${obj.title ?? ''}" text="${obj.text ?? ''}" image="${obj.image ?? ''}" >}}`;
-    },
-    toPreview: function (obj) {
-        return `{{< blockquote quote="${obj.quote ?? ''}" title="${obj.title ?? ''}" text="${obj.text ?? ''}" image="${obj.image ?? ''}" >}}`;
-    }
+  id: 'blockquote',
+  label: t.shortcodes.blockquote,
+  fields: [
+    quote,
+    title,
+    text,
+    image_src
+  ],
+  pattern: /{{< blockquote quote="(.*?)" title="(.*?)" text="(.*?)" image="(.*?)" >}}/,
+  fromBlock: function (match) {
+    return {
+      quote: match[1] ?? '',
+      title: match[2] ?? '',
+      text: match[3] ?? '',
+      image: match[4] ?? ''
+    };
+  },
+  toBlock: function (obj) {
+    return `{{< blockquote quote="${obj.quote ?? ''}" title="${obj.title ?? ''}" text="${obj.text ?? ''}" image="${obj.image ?? ''}" >}}`;
+  },
+  toPreview: function (obj) {
+    return `
+      <figure class="quote">
+        <blockquote>
+          <p>${obj.quote ?? ''}</p>
+        </blockquote>
+        <figcaption>
+          <picture>
+            ${obj.image ?? ''}
+          </picture>
+          <div>
+            <cite>${obj.title ?? ''}</cite><br>
+            ${obj.text ?? ''}
+          </div>
+        </figcaption>
+      </figure>
+    `;
+  }
 });
