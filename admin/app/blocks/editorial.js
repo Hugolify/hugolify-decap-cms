4ff86f5de038bb0283af8aaacd32356153a4a0fe
sebousan <sebousan@gmail.com>
2023-12-12 12:14:41 +0100

    feat: add notes to editorial block

--- admin/app/blocks/editorial.js
+++ admin/app/blocks/editorial.js
@@ -1,10 +1,11 @@
-import { direction } from '../fields/direction.js';
-import { surtitle } from '../fields/surtitle.js';
-import { title } from '../fields/title.js';
-import { text_markdown } from '../fields/text-markdown.js';
-import { cta } from '../fields/cta.js';
-import { image } from '../fields/image.js';
 import { background } from '../fields/background.js';
+import { cta } from '../fields/cta.js';
+import { direction } from '../fields/direction.js';
+import { image } from '../fields/image.js';
+import { notes } from '../fields/notes.js';
+import { surtitle } from '../fields/surtitle.js';
+import { text_markdown } from '../fields/text-markdown.js';
+import { title } from '../fields/title.js';
 import { t } from '../i18n/translater.js';
 
 export const block_editorial = {
@@ -15,5 +16,14 @@ export const block_editorial = {
   i18n: true,
   collapsed: false,
   summary: '{{title}}',
-  fields: [direction, surtitle, title, text_markdown, cta, image, background]
+  fields: [
+    direction,
+    surtitle,
+    title,
+    text_markdown,
+    notes,
+    cta,
+    image,
+    background
+  ]
 };
