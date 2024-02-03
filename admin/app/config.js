be67512d1e986bf8b76b6c96aee302361d1da155
Arthaud <arthaudp33@gmail.com>
2024-02-03 22:55:02 +0100

    feat: add publish_mode config

--- admin/app/config.js
+++ admin/app/config.js
@@ -1,19 +1,21 @@
 import backend from './config/backend.js';
+import display_url from './config/display_url.js';
+import i18n from './config/i18n.js';
 import load_config_file from './config/load_config_file.js';
 import local_backend from './config/local_backend.js';
-import logo_url from './config/logo_url.js';
-import media_library from './config/media_library.js';
-import media_folder from './config/media_folder.js';
-import public_folder from './config/public_folder.js';
-import display_url from './config/display_url.js';
 import locale from './config/locale.js';
-import i18n from './config/i18n.js';
+import logo_url from './config/logo_url.js';
+import media_folder from './config/media_folder.js';
+import media_library from './config/media_library.js';
+import public_folder from './config/public_folder.js';
+import publish_mode from './config/publish_mode.js';
 import slug from './config/slug.js';
 
-import { mediaLibrary } from './settings/media-library.js';
 import { availableLanguages } from './settings/available-languages.js';
+import { mediaLibrary } from './settings/media-library.js';
 
 export let config = {
+  publish_mode,
   backend,
   load_config_file,
   local_backend,
