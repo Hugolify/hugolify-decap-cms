a123d573731e62307f94c704f2c9a744494719de
sebousan <sebousan@gmail.com>
2025-01-13 21:33:30 +0100

    add language field

---
+++ admin/app/fields/language.js
@@ -0,0 +1,10 @@
+import { t } from '../i18n/translater.js';
+
+export const language = {
+  name: 'language',
+  label: t.fields.language.label,
+  hint: t.fields.language.hint,
+  widget: 'string',
+  required: false,
+  i18n: true
+};
